05a5bd6c4ce04a9b102270b8a0364b549814d311
Ahmed Salama <ahmedsalamafcis@gmail.com>
2026-02-23 09:33:39 +0200

    Was able to get the data from the api and display it in the frontend

--- src/app/components/services/services.ts
+++ src/app/components/services/services.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, inject, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-services',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './services.html',
   styleUrl: './services.css',
 })
-export class Services {
+export class Services implements OnInit {
+  apiService = inject(ApiService);
+  services: any[] = [];
 
+  ngOnInit(): void {
+    this.apiService.getServices().subscribe((data: any) => {
+      this.services = data;
+      console.log(this.services);
+    });
+  }
 }
+
+
